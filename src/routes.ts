1d6870cbfbfd32e057484bad311b59c8eb01d020
marcosps1 <marcos.santos@nwaypro.com>
2024-12-02 00:17:31 -0300

    Interface, Controller, Service e rota de Deletar Categoria
    Instalando e configurando a biblioteca Multer, para upload de imagens
    
    yarn add multer
    yarn add @types/multer -D
    
    Interface, Controller, Service e rota de Criar Produto com imagem
    
    Interface, Controller, Service e rota de Editar Produto com imagem
    
    Interface, Controller, Service e rota para Buscar Produto por categoria
    
    Interface, Controller, Service e rota para Buscar Produtos
    
    Interface, Controller, Service e rota para Deletar Produto
    
    Interface, Controller, Service e rota para Vender Produto

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
 import { Router, Request, Response } from "express";
+import multer from "multer";
+import uploadConfig from "./config/multer";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
@@ -7,8 +9,17 @@ import { RemoveUserController } from "./controllers/user/RemoveUserController";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { EditCategoryController } from "./controllers/category/EditCategoryController";
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
+import { RemoveCategoryController } from "./controllers/category/RemoveCategoryController";
+import { CreateProductController } from "./controllers/product/CreateProductController";
+import { EditProductController } from "./controllers/product/EditProductController";
+import { ListProductByCategoryController } from "./controllers/product/ListProductByCategoryController";
+import { ListProductsController } from "./controllers/product/ListProductsController";
+import { RemoveProductController } from "./controllers/product/RemoveProductController";
+import { SaleProductController } from "./controllers/sale/SaleProductController";
 
 const router = Router();
+const upload = multer(uploadConfig.upload("./tmp"));
+
 router.get("/test", (request: Request, response: Response) => {
   return response.json({ ok: true });
 });
@@ -34,6 +45,43 @@ router.get(
   "/category/all",
   isAuthenticated,
   new ListCategoryController().handle,
+);
+router.delete(
+  "/category/remove",
+  isAuthenticated,
+  new RemoveCategoryController().handle,
+);
+
+// Product Routes
+router.post(
+  "/product",
+  isAuthenticated,
+  upload.single("file"),
+  new CreateProductController().handle,
+);
+router.put(
+  "/product/edit",
+  isAuthenticated,
+  upload.single("file"),
+  new EditProductController().handle,
+);
+router.get(
+  "/product",
+  isAuthenticated,
+  new ListProductByCategoryController().handle,
+);
+router.get("/products", isAuthenticated, new ListProductsController().handle);
+router.delete(
+  "/product/remove",
+  isAuthenticated,
+  new RemoveProductController().handle,
+);
+
+// Sale Routes
+router.put(
+  "/sale/product",
+  isAuthenticated,
+  new SaleProductController().handle,
 );
 
 export { router };
